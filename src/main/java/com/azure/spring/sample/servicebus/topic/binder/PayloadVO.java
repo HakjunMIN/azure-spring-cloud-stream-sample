26aaa6f96bc2de423f2f12dd8699aba8b716cfd3
Andrew Min <andrewmin@microsoft.com>
2022-01-26 16:49:18 +0900

    Seperate producer and consumer, elaborate document

--- src/main/java/com/azure/spring/sample/servicebus/topic/binder/PayloadVO.java
+++ src/main/java/com/azure/spring/sample/servicebus/topic/binder/PayloadVO.java
@@ -1,11 +1,14 @@
 package com.azure.spring.sample.servicebus.topic.binder;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PayloadVO {
+    @JsonProperty
     private String message;
+    @JsonProperty
     private String name;
 }
--- src/main/java/com/azure/spring/sample/servicebus/topic/binder/ServiceProducerController.java
+++ src/main/java/com/azure/spring/sample/servicebus/topic/binder/ServiceProducerController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.databind.ObjectMapper;
